ba7a876cfc3a9987b277b0248e6e5976f9418dd0
Brian M Hunt <brianmhunt@gmail.com>
2013-12-04 10:57:39 -0500

    Travis/Sauce dance - try webdriverjs
    Not that webdriverjs works with chromedriver …

--- spec/runner.js
+++ spec/runner.js
@@ -9,43 +9,51 @@
 //  Some handy webdriver docs
 //  https://code.google.com/p/selenium/wiki/WebDriverJs
 //
+'use strict'
 require('colors')
 
-var webdriver = require('selenium-webdriver'),
+var webdriverjs = require('webdriverjs'),
     server = require("./server"),
 
-    // our Sauce webdriver url
-    hub_url,
-
-    // what our webdriver may provide
+    // our webdriver desired capabilities
     capabilities,
 
-    // sauce options
-    sauce,
+    // what our webdriver may provide
+    client,
 
     // we use this for ensuring the document is loaded, below
     expect_title = "Knockout Secure Binding - Local unit tests"
+
+console.log("WJS")
+
+capabilities = {
+  browserName: "chrome"
+}
 
 if (process.env['SAUCE_USERNAME']) {
   // use sauce; see
   // eg http://about.travis-ci.org/docs/user/gui-and-headless-browsers/
   console.log("\nTesting with Sauce Labs".bold)
-  capabilities = webdriver.Capabilities.chrome()
 
   capabilities["build"] = process.env['TRAVIS_BUILD_NUMBER']
+  capabilities["javascriptEnabled"] = true
 
-  hub_url = "" + process.env['SAUCE_USERNAME']
-    + ":" + process.env['SAUCE_ACCESS_KEY']
-    + "@localhost:4445"
+  client = webdriverjs.remote({
+    host: "localhost",
+    port: 4445,
+    user: process.env.SAUCE_USERNAME,
+    key: process.env.SAUCE_ACCESS_KEY,
+    desiredCapabilities: capabilities
+  })
 
-  console.log("Sauce HUB URL ", hub_url)
-
-  driver = webdriver.Remote(desired_capabilities=capabilities,
-    command_executor="http://" + hub_url + "/wd/hub")
 } else {
   console.log("\nTesting with local chromedriver".bold)
-  driver = new webdriver.Builder().
-    withCapabilities(webdriver.Capabilities.chrome())
+
+  client = webdriverjs.remote({
+    host: "localhost",
+    port: 9515,
+    desiredCapabilities: capabilities
+  })
 }
 
 
@@ -53,46 +61,44 @@ function run_browser_tests() {
   var uri = 'http://' + server.host + ":" + server.port,
     remote_script = "return window.tests",
     results = false,
+    WAIT = 1000,
     fails = 0;
 
-  var client = driver.build();
-
-  client.get(uri)
-
-  // wait for title to load - make sure we're at the right spot
-  client.wait(function() {
-   return client.getTitle().then(function(title) {
-     return title === expect_title;
-   })
-  }, 1000)
-
-  // let the scripts run and get the mocha test results
-  client.wait(function () {
-    return client.executeScript(remote_script).then(function (res) {
-      results = res;
-      return res.complete;
-    })
-  }, 1000).then(function () {
-    results.results.forEach(function (result) {
-      var state = result.state;
-      if (state !== 'passed') {
-        fails++
-        state = state.red.bold
-      } else {
-        state = state.blue
+  client.url(uri)
+    .pause(WAIT)
+    .getTitle(function (err, title) {
+      if (err) {
+        throw new Error(err)
       }
+      if (title !== expect_title) {
+        throw new Error("Expected title " + expect_title + " but got "
+          + title)
+      }
+    })
+    .execute(remote_script, null, function (err, res) {
+      if (err) {
+        throw new Error(err)
+      }
+      results = res;
+      results.results.forEach(function (result) {
+        var state = result.state;
+        if (state !== 'passed') {
+          fails++
+          state = state.red.bold
+        } else {
+          state = state.blue
+        }
 
-      console.log(state + "  " + result.title)
-    });
+        console.log(state + "  " + result.title)
+      });
 
-    console.log("Total: ", results.results.length, " fails: ", fails)
-  })
+      console.log("Total: ", results.results.length, " fails: ", fails)
 
-  // quit the client and exit with appropriate code
-  client.quit().then(function () {
-    // exit code 0 when there are no fails.
-    process.exit(fails)
-  });
+      // quit the client and exit with appropriate code
+      webdriverjs.endAll(function () {
+        process.exit(fails)
+      })
+    })
 }
 
 
