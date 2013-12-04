211732c36f9de8cde3d975fdb01d4d8f15dc4150
Brian M Hunt <brianmhunt@gmail.com>
2013-12-04 09:37:27 -0500

    Try Firefox webdriver for Travis CI

--- spec/runner.js
+++ spec/runner.js
@@ -1,12 +1,44 @@
 #!/usr/bin/env node
+//
+//  runner.js
+//  ---------
+//
+//  Automated testing of Knockout-Secure-Binding
+//
+//
+//  Some handy webdriver docs
+//  https://code.google.com/p/selenium/wiki/WebDriverJs
+//
 require('colors')
 
 var webdriver = require('selenium-webdriver'),
+    SeleniumServer = require('selenium-webdriver/remote').SeleniumServer,
     server = require("./server"),
-    capabilities = webdriver.Capabilities.chrome(),
+
+    // selenium setup for standalone servers
+    selenium_jar = "selenium-server-standalone-2.37.0.jar",
+    selenium_port = 4444,
+    selenium_server,
+
+    // this is used below for the driver
+    driver,
 
     // we use this for ensuring the document is loaded, below
     expect_title = "Knockout Secure Binding - Local unit tests"
+
+// we accept an argument of "firefox" to start Selenium
+if (process.argv[2] == "firefox") {
+  selenium_server = new SeleniumServer(selenium_jar,
+    { port: selenium_port })
+  selenium_server.start()
+  driver = new webdriver.Builder().
+    usingServer(selenium_server.address()).
+    withCapabilities(webdriver.Capabilities.firefox())
+} else {
+  driver = new webdriver.Builder().
+    withCapabilities(webdriver.Capabilities.chrome())
+}
+
 
 function run_browser_tests() {
   var uri = 'http://' + server.host + ":" + server.port,
@@ -14,21 +46,20 @@ function run_browser_tests() {
     results = false,
     fails = 0;
 
-  var driver = new webdriver.Builder()
-    .withCapabilities(capabilities).build();
+  var client = driver.build();
 
-  driver.get(uri)
+  client.get(uri)
 
   // wait for title to load - make sure we're at the right spot
-  driver.wait(function() {
-   return driver.getTitle().then(function(title) {
+  client.wait(function() {
+   return client.getTitle().then(function(title) {
      return title === expect_title;
    })
   }, 1000)
 
   // let the scripts run and get the mocha test results
-  driver.wait(function () {
-    return driver.executeScript(remote_script).then(function (res) {
+  client.wait(function () {
+    return client.executeScript(remote_script).then(function (res) {
       results = res;
       return res.complete;
     })
@@ -48,8 +79,8 @@ function run_browser_tests() {
     console.log("Total: ", results.results.length, " fails: ", fails)
   })
 
-  // quit the driver and exit with appropriate code
-  driver.quit().then(function () {
+  // quit the client and exit with appropriate code
+  client.quit().then(function () {
     // exit code 0 when there are no fails.
     process.exit(fails)
   });
