07b5d564782ebc5eb19ed8ae8909f61a7511239f
cervengoc <zolitamasi@gmail.com>
2015-07-14 11:21:21 +0200

    Add support for batch insertion if possible
    Adds support for batch insertion by using the more efficient
    DocumentFragment.

--- spec/knockout-fast-foreach-spec.js
+++ spec/knockout-fast-foreach-spec.js
@@ -14,13 +14,13 @@ assert = chai.assert;
 
 // Make the frame animation synchronous; simplifies testing.
 function setupSynchronousFrameAnimation () {
-  var originalAnimateFrame = FastForEach.animateFrame;
+  var originalAnimateFrame = ko.bindingHandlers.fastForEach.FastForEach.animateFrame;
   beforeEach(function () {
-    originalAnimateFrame = FastForEach.animateFrame;
-    FastForEach.animateFrame = function(frame) { frame() };
+    originalAnimateFrame = ko.bindingHandlers.fastForEach.FastForEach.animateFrame;
+    ko.bindingHandlers.fastForEach.FastForEach.animateFrame = function (frame) { frame() };
   })
   afterEach(function () {
-    FastForEach.animateFrame = originalAnimateFrame;
+    ko.bindingHandlers.fastForEach.FastForEach.animateFrame = originalAnimateFrame;
   })
 }
 
