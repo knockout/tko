e3376f82e2e8239a80cb6b569d142453c45266c0
Brian M Hunt <brianmhunt@gmail.com>
2016-01-05 09:56:59 -0500

    Initial toolset with `init` task

---
+++ tasks/init.js
@@ -0,0 +1,73 @@
+//
+// -- init -- task
+//
+var fs = require('fs')
+var path = require('path')
+var child_process = require('child_process')
+
+var _ = require('lodash')
+var gulp = require('gulp')
+var inquirer = require('inquirer')
+
+var cwd = process.cwd()
+var pkg = require(`${cwd}/package.json`)
+var template_dir = path.join(__dirname, '../templates')
+
+// The following are files created/updated in the package directory, from
+// the /templates directory
+var FILES = {
+  LICENSE: 'LICENSE',
+  'config.yaml': 'config.yaml',
+  'circle.yml': 'circle.yml',
+  eslintrc: '.eslintrc',
+  editorconfig: '.editorconfig',
+  gitignore: '.gitignore',
+  npmignore: '.npmignore',
+}
+
+// Template variables
+// License: {{ year }} / {{ pkg.author }}
+// README.md: ...
+
+gulp.task('init', function () {
+  // Gather bits of info first.
+  if (_.get(pkg, 'tko.blacklist')) {
+    throw new Error(`tko.tools cannot init ${pkg.name} because it is blacklisted in package.json.`)
+  }
+
+  var modified_files_cmd = `git ls-files -mo ${_.values(FILES).join(" ")}`
+  var modified_files = child_process.execSync(
+    modified_files_cmd, {encoding: 'utf8'})
+  console.log("MFC", modified_files_cmd)
+
+  if (modified_files !== '') {
+    throw new Error(`There are modified files: ${modified_files}`)
+  }
+
+  _.each(FILES, write_template_file)
+})
+
+function exists(path) {
+  // See eg http://stackoverflow.com/questions/4482686
+  try {
+    fs.accessSync(path, fs.F_OK);
+    return true
+  } catch (e) {
+    return false
+  }
+}
+
+
+function write_template_file(dst_filename, src_filename) {
+  var dst_path = path.join(cwd, dst_filename)
+  var src_path = path.join(template_dir, src_filename)
+  var template = fs.readFileSync(src_path, {encoding: 'utf8'})
+  var compiled = _.template(template)({ pkg: pkg, now: new Date() })
+
+  if (exists(dst_path)) {
+    console.log(`Overwriting ${dst_path.underline}.`)
+  }
+  fs.writeFileSync(dst_path, compiled)
+}
+
+gulp.start('init')
