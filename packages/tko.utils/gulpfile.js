2bd26608c752a59dfb5001985363401a7eb8031f
Brian M Hunt <brianmhunt@gmail.com>
2016-01-05 10:44:13 -0500

    Switch to using tko.tools for gulp tasks

--- packages/tko.utils/gulpfile.js
+++ packages/tko.utils/gulpfile.js
@@ -1,17 +1,1 @@
-//
-// Gulp tasks
-//
-var gulp = require('gulp')
-var rollup = require('rollup')
-
-gulp.task('make', function () {
-  rollup.rollup({
-    entry: 'index.js',
-  }).then(function(bundle) {
-    return bundle.write({
-      format: 'umd',
-      moduleName: 'tko.utils',
-      dest: 'dist/tko.utils.js'
-    });
-  })
-})
+require('tko.tools')(require('gulp'))
