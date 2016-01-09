12a156b3069e63d37bf37a139820861f16523702
Brian Hunt <brianmhunt@gmail.com>
2016-01-08 19:05:22 -0500

    🎨 Expose variables for setting legacy utilities

--- packages/tko.utils/src/obs.js
+++ packages/tko.utils/src/obs.js
@@ -1,15 +1,9 @@
 //
 // Observables
+// ---
 //
-
-
-export function unwrapObservable (value) {
-    return ko.isObservable(value) ? value() : value;
-}
-
-export function peekObservable (value) {
-    return ko.isObservable(value) ? value.peek() : value;
-}
-
-// Shorten `unwrap` alias
-export var unwrap = unwrapObservable
+// If `tko.observable` is included, then these will be set to `unwrap` and
+// `peek`.
+//
+export var unwrapObservable
+export var peekObservable
