6ddcdd2e6adf1aa032843c4ca35edb28cb8cb805
Brian M Hunt <brianmhunt@gmail.com>
2016-02-23 10:54:32 -0500

    Break out tko.bind, tko.computed, tko.components

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import * as utils from 'tko.utils'
 import * as observable from 'tko.observable'
 import * as extenders from './src/defaultExtenders'
 
-ko.utils = utils
+ko.utils = {}
+utils.extend(ko.utils, utils)
+utils.extend(ko.utils, {
+  unwrapObservable: observable.unwrap,
+  peekObservable: observable.peek
+})
 utils.extend(ko, observable)
-
 utils.extend(observable.extenders, extenders)
