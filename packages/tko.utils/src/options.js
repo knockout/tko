98da8f6ed3f33fd13a22aeeee89ff6ce5e7ce4d0
Brian M Hunt <bmh@tko.io>
2016-07-12 09:41:06 -0400

    Add jQuery option

--- packages/tko.utils/src/options.js
+++ packages/tko.utils/src/options.js
@@ -8,6 +8,11 @@ export default {
     deferUpdates: false,
     useOnlyNativeEvents: false,
     protoProperty: '__ko_proto__',
+
+    // jQuery will be automatically set to window.jQuery in applyBindings
+    // if it is (strictly equal to) undefined.  Set it to false or null to
+    // disable automatically setting jQuery.
+    jQuery: undefined,
     debug: false,
     $: window && window.jQuery,
     onError: function (e) { throw e; }
