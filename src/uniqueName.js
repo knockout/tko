33a9ece2789ee11b4c5d7c612aa91ae68ab8ff17
Brian Hunt <brianmhunt@gmail.com>
2016-07-21 15:25:10 -0400

    import of setElementName

--- src/uniqueName.js
+++ src/uniqueName.js
@@ -1,11 +1,13 @@
 
-
+from {
+    setElementName
+} import 'tko.utils';
 
 export var uniqueName = {
     init: function (element, valueAccessor) {
         if (valueAccessor()) {
             var name = "ko_unique_" + (++uniqueName.currentIndex);
-            ko.utils.setElementName(element, name);
+            setElementName(element, name);
         }
     },
     currentIndex: 0
