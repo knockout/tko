e2c75717df8d23b1874dde46177ffbb8b3e8804f
Kartheek Lenkala <lkarthee@gmail.com>
2016-09-11 17:32:58 +0530

    comment template let

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import {$if, $with, ifnot} from './src/ifIfnotWith'
-import {template} from './src/template'
+//import {template} from './src/template'
+//    'let': letBinding,
+//    template: template,
 
 export var bindings = {
     'if': $if,
     'with': $with,
     ifnot: ifnot, unless: ifnot,
-    'let': letBinding,
-    template: template,
 };
