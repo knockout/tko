ccef979c1603853cc50ed18efeb818f94d16f95b
Brian M Hunt <bmh@tko.io>
2016-09-22 11:42:23 -0400

    Remove unused JSON posting test/build test

--- spec/buildBehaviour.js
+++
@@ -1,9 +0,0 @@
-
-describe("Build behaviour", function() {
-	it('Should be aliased as ko.unwrap', function() {
-	    expect(ko.unwrap).toBe(ko.utils.unwrapObservable);
-		expect(ko.unwrap).toBe(ko.utils.unwrapObservable);
-	    expect(ko.unwrap(ko.observable('some value'))).toBe('some value');
-		expect(ko.unwrap(ko.observable('some value'))).toBe('some value');
-    });
-});
