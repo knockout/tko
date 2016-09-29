d428d2e8b70baa5a6f6bedd116236207474e6cbb
Brian M Hunt <bmh@tko.io>
2016-09-29 14:56:56 -0400

    Add virtualElement support to `html` binding

--- src/html.js
+++ src/html.js
@@ -1,15 +1,34 @@
+import {
+    setHtml, parseHtmlFragment, virtualElements
+} from 'tko.utils';
 
 import {
-    setHtml
-} from 'tko.utils';
+    unwrap
+} from 'tko.observable';
 
 export var html = {
     init: function() {
         // Prevent binding on the dynamically-injected HTML (as developers are unlikely to expect that, and it has security implications)
-        return { 'controlsDescendantBindings': true };
+        return {
+            'controlsDescendantBindings': true
+        };
     },
-    update: function (element, valueAccessor) {
+    //
+    // Modify internal, per ko.punches and :
+    //      http://stackoverflow.com/a/15348139
+    update: function(element, valueAccessor) {
+        if (element.nodeType === 8) {
+            var html = unwrap(valueAccessor());
+            if (html !== null) {
+                var parsedNodes = parseHtmlFragment('' + html);
+                virtualElements.setDomNodeChildren(element, parsedNodes);
+            } else {
+                virtualElements.emptyNode(element);
+            }
+        }
+
         // setHtml will unwrap the value if needed
         setHtml(element, valueAccessor());
-    }
+    },
+    allowVirtualElements: true
 };
