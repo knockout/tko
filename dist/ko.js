f2391ddb09b5baa685e8fb19032c7696ef0a3001
Brian M Hunt <bmh@tko.io>
2016-11-09 10:46:35 -0500

    For backwards compat, rename the package to `ko`

--- dist/ko.js
+++ dist/ko.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
-  (global.tko = factory());
+  (global.ko = factory());
 }(this, function () { 'use strict';
 
   //
--- gulpfile.js
+++ gulpfile.js
@@ -6,43 +6,5 @@ const cp = require('child_process')
 var gulp = require('gulp')
 require('tko-policy')(gulp)
 
-const S_INHERIT = { encoding: 'utf8', stdio: 'inherit' }
 
 
-function repoUrl(project) {
-    if (!project.includes('/')) {
-        // Project is "plain"; being loaded from npm.
-        return
-    }
-    if (!project.startsWith('https:') && !project.startsWith('git@')) {
-        project = project.replace(/^github:/, "")
-        if (!project.endsWith('.git')) { project += '.git' }
-        return `https://github.com/` + project
-    }
-    return project
-}
-
-
-gulp.task("submodules", () => {
-    let pkg = require('./package.json');
-    for (let subpackage in pkg.dependencies) {
-        var project = repoUrl(pkg.dependencies[subpackage]);
-        if (!project) {
-            console.log(`Skipping non-repo: ${subpackage}.`)
-            continue
-        }
-        let args = ["submodule", "add", repoUrl(project)]
-        console.log("  $ ", `git ${args.join(" ")}`)
-        cp.spawnSync("git", args, S_INHERIT)
-    }
-})
-
-
-gulp.task("submodules:update", () => {
-    let args = [
-        "submodules",
-        "update",
-        "--remote"
-    ]
-    cp.spawnSync("git", args)
-})
