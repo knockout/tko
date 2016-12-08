686676b18c4f41f8ded5986e82058ae1606e2279
Brian M Hunt <bmh@tko.io>
2016-12-08 15:22:22 -0500

    Enable tests of the filters (not generic filtering mechanism though)

--- packages/tko.filter.punches/spec/filterBehavior.js
+++ packages/tko.filter.punches/spec/filterBehavior.js
@@ -1,24 +1,39 @@
 
 
-describe("Text filters preprocessor", function() {
-    var filterPreprocessor = ko.punches.textFilter.preprocessor;
+import {
+  observable
+} from 'tko.observable'
 
-    it('Should convert basic input|filter syntax into function calls', function() {
+import { filters } from '../index'
+
+if (!Array.isArray) {
+  Array.isArray = function(arg) {
+    return Object.prototype.toString.call(arg) === '[object Array]';
+  };
+}
+
+
+describe("Text filters preprocessor", function() {
+    // var filterPreprocessor = ko.punches.textFilter.preprocessor;
+
+    /* Skipping the following five since they are tests that apply to the
+       filter-runner moreso than any specific filter exported here. */
+    xit('Should convert basic input|filter syntax into function calls', function() {
         expect(filterPreprocessor('input|filter1|filter2:arg|filter3:arg1:arg2'))
             .toEqual("ko.filters['filter3'](ko.filters['filter2'](ko.filters['filter1'](input),arg),arg1,arg2)");
     });
 
-    it('Should not be confused by | in quotes, or : or || in input', function() {
+    xit('Should not be confused by | in quotes, or : or || in input', function() {
         expect(filterPreprocessor('input1||(input2?a:b)||"A|B"|filter'))
             .toEqual("ko.filters['filter'](input1||(input2?a:b)||\"A|B\")");
     });
 
-    it('Should tolerate spaces or newlines in the input', function() {
+    xit('Should tolerate spaces or newlines in the input', function() {
         expect(filterPreprocessor('input \n| filter\n | filter2'))
             .toEqual("ko.filters['filter2'](ko.filters['filter'](input))");
     });
 
-    it('Should be able to run filter output and apply filters to input', function() {
+    xit('Should be able to run filter output and apply filters to input', function() {
         try {
             ko.filters.test = function(input) { return input.toUpperCase(); };
             expect(eval(filterPreprocessor('"someText" | test'))).toEqual('SOMETEXT');
@@ -27,7 +42,7 @@ describe("Text filters preprocessor", function() {
         }
     });
 
-    it('Should pass arguments when applying filter', function() {
+    xit('Should pass arguments when applying filter', function() {
         try {
             ko.filters.test = function(input, length) { return input.slice(0, length);             };
             expect(eval(filterPreprocessor('"someText" | test:5'))).toEqual('someT');
@@ -35,79 +50,84 @@ describe("Text filters preprocessor", function() {
             delete ko.filters.test;
         }
     });
+    
+    /* ~~~ Filters ~~~ */
+    
+    function attempt(filter, args, expected) {
+      expect(
+        filters[filter].apply(null, Array.isArray(args) ? args : [args])
+      ).toEqual(expected)
+    }
 
     it('Should convert text to uppercase using uppercase filter', function() {
-        expect(eval(filterPreprocessor('"someText" | uppercase'))).toEqual('SOMETEXT');
-        // Should unwrap observable value
-        expect(eval(filterPreprocessor('ko.observable("someText") | uppercase'))).toEqual('SOMETEXT');
+        attempt('uppercase', 'someText', 'SOMETEXT')
+        attempt('uppercase', observable('someText'), 'SOMETEXT')
     });
 
     it('Should convert text to lowercase using lowercase filter', function() {
-        expect(eval(filterPreprocessor('"someText" | lowercase'))).toEqual('sometext');
-        // Should unwrap observable value
-        expect(eval(filterPreprocessor('ko.observable("someText") | lowercase'))).toEqual('sometext');
+      attempt('lowercase', 'someText', 'sometext')
+      attempt('lowercase', observable('someText'), 'sometext')
     });
 
     it('Should convert blank text, empty array, or null to default value using default filter', function() {
         // non-blank value is not converted
-        expect(eval(filterPreprocessor('"someText" | default:"blank"'))).toEqual('someText');
+        attempt('default', ['someText', 'blank'], 'someText')
         // empty string value is converted
-        expect(eval(filterPreprocessor('"" | default:"blank"'))).toEqual('blank');
+        attempt('default', ['', 'blank'], 'blank')
         // string value with only spaces is converted
-        expect(eval(filterPreprocessor('"  " | default:"blank"'))).toEqual('blank');
+        attempt('default', ['   ', 'blank'], 'blank')
         // spaces around string value are not trimmed
-        expect(eval(filterPreprocessor('" x " | default:"blank"'))).toEqual(' x ');
+        attempt('default', ['  x  ', 'blank'], '  x  ')
         // zero value is not converted
-        expect(eval(filterPreprocessor('0 | default:"blank"'))).toEqual(0);
+        attempt('default', [0, "blank"], 0);
         // empty array is converted
-        expect(eval(filterPreprocessor('[] | default:"blank"'))).toEqual('blank');
+        attempt('default', [[], "blank"], 'blank');
         // null or undefined value is converted
-        expect(eval(filterPreprocessor('null | default:"blank"'))).toEqual('blank');
-        expect(eval(filterPreprocessor('undefined | default:"blank"'))).toEqual('blank');
+        attempt('default', [null, "blank"], 'blank');
+        attempt('default', [undefined, "blank"], 'blank');
         // a function is not converted
         var emptyFunction = function() {};
-        expect(eval(filterPreprocessor('emptyFunction | default:"blank"'))).toEqual(emptyFunction);
+        attempt('default', [emptyFunction, "blank"], emptyFunction);
         // Should unwrap observable value
-        expect(eval(filterPreprocessor('ko.observable("someText") | default:"blank"'))).toEqual('someText');
+        attempt('default', [observable("someText"), "blank"], 'someText');
     });
 
     it('Should replace found text in input using replace filter', function() {
-        expect(eval(filterPreprocessor('"someText" | replace:"some":"any"'))).toEqual('anyText');
+        attempt('replace', ["someText", 'some', 'any'], 'anyText') 
         // Should unwrap observable value
-        expect(eval(filterPreprocessor('ko.observable("someText") | replace:"some":"any"'))).toEqual('anyText');
+        attempt('replace', [observable("someText"), 'some', 'any'], 'anyText')
     });
 
     it('Should truncate text if appropriate using fit filter', function() {
         // Does nothing if input is shorter than length
-        expect(eval(filterPreprocessor('"someText" | fit:8'))).toEqual('someText');
+        attempt('fit', ["someText", 8], 'someText');
         // Truncates and add ellipses if input is longer than length
-        expect(eval(filterPreprocessor('"someText" | fit:7'))).toEqual('some...');
+        attempt('fit', ["someText", 7], 'some...');
         // Truncates and add custom text if specified
-        expect(eval(filterPreprocessor('"someText" | fit:7:"!"'))).toEqual('someTe!');
+        attempt('fit', ["someText", 7, "!"], 'someTe!')
         // Truncates from the left if specified
-        expect(eval(filterPreprocessor('"someText" | fit:7::"left"'))).toEqual('...Text');
+        attempt('fit', ["someText", 7, undefined, "left"], '...Text')
         // Truncates in the middle if specified
-        expect(eval(filterPreprocessor('"someText" | fit:7::"middle"'))).toEqual('so...xt');
+        attempt('fit', ["someText", 7, undefined, "middle"], 'so...xt')
         // Should unwrap observable value
-        expect(eval(filterPreprocessor('ko.observable("someText") | fit:8'))).toEqual('someText');
+        attempt('fit', [observable("someText"), 8], 'someText');
     });
 
     it('Should convert input to JSON text using json filter', function() {
         // Converts string
-        expect(eval(filterPreprocessor('"someText" | json'))).toEqual('"someText"');
+        attempt('json', "someText", '"someText"');
         // Converts array
-        expect(eval(filterPreprocessor('[1,2,3] | json'))).toEqual('[1,2,3]');
+        attempt('json', [[1,2,3]], '[1,2,3]');
         // Converts object
-        expect(eval(filterPreprocessor('{a:true, b:false, c:null} | json'))).toEqual('{"a":true,"b":false,"c":null}');
+        attempt('json', {a:true, b:false, c:null}, '{"a":true,"b":false,"c":null}');
         // Accepts space argument
-        expect(eval(filterPreprocessor('{a:true, b:false, c:null} | json:" "'))).toEqual('{\n "a": true,\n "b": false,\n "c": null\n}');
+        attempt('json', [{a:true, b:false, c:null}, 1], '{\n "a": true,\n "b": false,\n "c": null\n}');
     });
 });
 
-describe("Text filter bindings", function() {
+/* Skip this since */
+xdescribe("Text filter bindings", function() {
     beforeEach(jasmine.prepareTestNode);
-    beforeEach(function() { ko.punches.textFilter.enableForBinding('text'); });
-    afterEach(function() { ko.bindingHandlers.text.preprocess = null; });
 
     it('Should convert input into appropriate output', function() {
         testNode.innerHTML = "<div data-bind='text: input | lowercase | fit:10 | json'></div>";
