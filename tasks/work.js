b57e3506bccd3a2147895ad9a9d38c0fcd75c833
Brian M Hunt <bmh@tko.io>
2017-02-27 12:43:22 -0500

    Begin `work:*` tasks

---
+++ tasks/work.js
@@ -0,0 +1,114 @@
+
+const _ = require('lodash')
+const fs = require('fs')
+const resolve = require('resolve')
+const {execSync} = require('child_process')
+
+const UTF = { encoding: 'utf-8' }
+const WORK_DIR = 'work/'
+const GIT_OPTS = { cwd: WORK_DIR }
+const gulp = global.__tko_gulp
+const work = global.config.work || {
+  packages: /tko\..*/
+}
+
+function throwIfErr(err) {
+  if (err) { throw err }
+}
+
+function repoPkg(repo) {
+  // return JSON.parse(fs.readFileSync(`node_modules/${repo}/package.json`, UTF))
+  var pkg = null
+  resolve.sync(repo, {
+    basedir: process.cwd(),
+    packageFilter: (p, f) => pkg = p
+  })
+  return pkg
+}
+
+/**
+ * There are several forms that the URL will come in.  Some may require
+ * usernames + passwords (git:), others will straight-up not work (git+https:)
+ *
+ * Sample URL:
+ * 
+ *  git+https://github.com/knockout/tko.binding.core.git
+ */
+function canonRepoUrl(url) {
+  return url.replace('git+', '')
+    .replace('git:', 'https:')
+}
+
+
+function alreadyCloned(pkgName) {
+  return fs.existsSync(WORK_DIR + pkgName)
+}
+
+
+function gitAction(icon, action, pkgName, opts) {
+  const repoUrl = canonRepoUrl(repoPkg(pkgName).repository.url)
+  const cmd = `git ${action} ${repoUrl}`
+  console.log(`${icon}  ${action}    ${pkgName.blue} at ${repoUrl}`)
+  return execSync(cmd, opts)
+}
+
+
+gulp.task('work:mkdir', () => {
+  try {
+    fs.mkdirSync(WORK_DIR)
+  } catch(err) { if (err.code !== "EEXIST") { throw e } }
+})
+
+
+gulp.task('work:clone', ['work:mkdir'], () => {
+  return _(global.pkg.dependencies)
+    .pickBy((remote, pkgName) =>
+      work.packages.test(pkgName) && !alreadyCloned(pkgName)
+    )
+    .each((remote, pkgName) =>
+      gitAction('🌻', 'clone', pkgName, GIT_OPTS)
+    )
+})
+
+
+gulp.task('work:pull', ['work:mkdir'], () => {
+  return fs.readdirSync(WORK_DIR)
+    .forEach((pkgName) =>
+      gitAction('⬆️', 'pull', pkgName,
+        Object.assign({cwd: WORK_DIR + pkgName})
+      )
+    )
+})
+
+
+const DIFF_CMD = 'git diff HEAD'
+const AHEAD_BEHIND_CMD = 'git rev-list --left-right --count master...origin/master'
+
+gulp.task('work:status', () => {
+  const PATHS = fs.readdirSync(WORK_DIR)
+  const longest = _.maxBy(PATHS, (p) => p.length).length
+  
+  return PATHS.forEach((path) => {
+      const OPTS = Object.assign({}, UTF, { cwd: WORK_DIR + path })
+      const modified = Boolean(execSync(DIFF_CMD, OPTS))
+      const [ahead, behind] = execSync(AHEAD_BEHIND_CMD, OPTS).split(/\s+/)
+      const outdated = ahead !== behind || ahead !== '0'
+      
+      const mColor = modified ? 'red' : outdated ? 'yellow' : 'green'
+      const mText = modified ? 'modified'.red : ''
+      const aText = _.parseInt(ahead) ? `${ahead.blue} ahead of remote` : ''
+      const bText = _.parseInt(behind) ? `${behind.blue} behind remote` : ''
+      const pathStr = _.padStart(path, longest)
+      const texts = [aText, bText, mText].filter((v) => v).join(', ') || 'ok'
+      
+      console.log(`${pathStr[mColor]} ${texts}`)
+    })
+})
+
+
+// gulp.task('work:prune', ['work:mkdir'], () => {
+//   Remove any directories not in package.json.
+// })
+
+
+gulp.task('work:update', ['work:pull', 'work:clone'], _.noop)
