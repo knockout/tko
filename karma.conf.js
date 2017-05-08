fa2035ab7db60feed4496735d71b801c57b88021
Brian M Hunt <brianmhunt@gmail.com>
2017-05-08 17:37:34 -0400

    Karma - Set up config for running tests in each package
    Also:
    
    - remove extra files from tko.lifecycle
    - remove policy test task

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,56 @@
 /* eslint semi: 0 */
-var fs = require('fs')
-var yaml = require('js-yaml')
 
-module.exports = function (karmaConf) {
-    var config = yaml.safeLoad(fs.readFileSync('./config.yaml', 'utf8'));
-    var options = config.karma
-    options.files = options.files.concat(config.sources, config.spec_files)
-    // options.browsers ...
-    karmaConf.set(options)
+const fs = require('fs')
+const path = require('path')
+
+const nodeResolve = require('rollup-plugin-node-resolve')
+const rollupCommonJs = require('rollup-plugin-commonjs')
+const rollupBabel = require('rollup-plugin-babel')
+
+const pkg = JSON.parse(fs.readFileSync('package.json'))
+
+const root = path.join(process.cwd(), 'spec')
+
+const frameworks = pkg.karma.frameworks
+const browsers = ['PhantomJS']
+
+const files = [
+  { pattern: "polyfills.js" },
+  { pattern: "src/*.js", included: false, watched: true },
+  { pattern: "spec/*.js" },
+]
+
+const preprocessors = {
+  'polyfills.js': ['rollup'],
+  'src/**/*.js': ['rollup'],
+  'spec/**/*.js': ['rollup'],
+}
+
+const ROLLUP_CONFIG = {
+  RESOLVE: {jsnext: true}, //, customResolveOptions: {moduleDirectory: "packages"}},
+  BABEL: {},
+  COMMONJS: {include: 'node_modules/**'},
+}
+
+const rollupPreprocessor = Object.assign({}, {
+  format: 'iife',
+  moduleName: pkg.name,
+  plugins: [
+      nodeResolve(ROLLUP_CONFIG.RESOLVE),
+      rollupCommonJs(ROLLUP_CONFIG.COMMONJS),
+      rollupBabel(ROLLUP_CONFIG.BABEL),
+  ],
+  sourceMap: process.argv.includes('--sourcemap') ? 'inline': false,
+})
+
+module.exports = (config) => {
+  config.set({
+    files,
+    preprocessors,
+    rollupPreprocessor,
+    frameworks,
+    browsers,
+    resolve: { root },
+    singleRun: process.argv.includes('--once')
+  })
 }
