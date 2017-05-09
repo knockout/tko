5e27f09b0e2fd556780799bfcfdb17c6288b67b9
Brian M Hunt <bmh@tko.io>
2017-05-08 22:26:57 -0400

    Add theoretical implementation of minification with babili

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import fs from 'fs'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import replace from 'rollup-plugin-replace'
 import rollupBabel from 'rollup-plugin-babel'
+import rollupBabili from 'rollup-plugin-babili'
 
 const pkg = JSON.parse(fs.readFileSync("package.json"))
 
@@ -10,7 +11,8 @@ const CONFIG = {
   /* Replace {{VERSION}} with pkg.json's `version` */
   REPLACE: {delimiters: ['{{', '}}'], VERSION: `"${pkg.version}"`},
   RESOLVE: {jsnext: true, customResolveOptions: {moduleDirectory: "packages"}},
-  BABEL: {exclude: 'node_modules/**'}
+  BABEL: {exclude: 'node_modules/**'},
+  BABILI: {sourceMap: true},
 }
 
 const plugins = [
@@ -20,6 +22,10 @@ const plugins = [
 
 if (process.env.USE_BABEL) {
   plugins.push(rollupBabel(CONFIG.BABEL))
+}
+
+if (process.env.MINIFY) {
+  plugins.push(rollupBabili(CONFIG.BABILI))
 }
 
 export default {
