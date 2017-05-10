79085baf6f3bf8652b1edd3fdc20a6b34953ef5c
Brian M Hunt <brianmhunt@gmail.com>
2017-05-10 17:26:07 -0400

    LifeCycle) Fix the `mixInto` functionality for ES6
    In ES6 (without polyfill/Babel/transpilation/etc), the `LifeCycle.prototype` properties (i.e. prototype properties of a Class) are not enumerable.
    
    To work around this we can use `Object.getOwnProperty{Names/Descriptors}` to do what is basically an `Object.assign` of non-enumerable properties.
    
    Turns out this issue of non-enumerable prototype ES6 class properties has been a source of some consternation and debate.  Most (all) of the polyfills I saw did not recognize this problem, and relied on `for-in`, which only works for enumerable properties - hence, not here.

--- packages/tko.lifecycle/src/LifeCycle.js
+++ packages/tko.lifecycle/src/LifeCycle.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 import {
     addDisposeCallback, createSymbolOrString
 } from 'tko.utils'
@@ -9,25 +8,32 @@ import {
     computed
 } from 'tko.computed'
 
-
 const SUBSCRIPTIONS = createSymbolOrString('LifeCycle Subscriptions List')
 const ANCHOR_NODE = createSymbolOrString('LifeCycle Anchor Node')
-
 
 export default class LifeCycle {
   // NOTE: For more advanced integration as an ES6 mixin, see e.g.:
   // http://justinfagnani.com/2015/12/21/real-mixins-with-javascript-classes/
 
-  static mixInto(Constructor) {
-    Object.assign(Constructor.prototype || Constructor, LifeCycle.prototype)
+  /**
+   * Copy the properties of the LifeCycle class to the target (or its prototype)
+   *
+   * NOTE: getOwnPropertyNames is needed to copy the non-enumerable properties.
+   */
+  static mixInto (Constructor) {
+    const target = Constructor.prototype || Constructor
+    const mixin = LifeCycle.prototype
+    for (let prop of Object.getOwnPropertyNames(mixin)) {
+      target[prop] = mixin[prop]
+    }
   }
 
-  subscribe(observable, action) {
+  subscribe (observable, action) {
     if (typeof action === 'string') { action = this[action].bind(this) }
     this.addDisposable(observable.subscribe(action))
   }
 
-  computed(params) {
+  computed (params) {
     if (typeof params === 'string') {
       params = { read: this[params], write: this[params], owner: this }
     } else if (typeof params === 'object') {
@@ -35,47 +41,46 @@ export default class LifeCycle {
     } else if (typeof params === 'function') {
       params = { read: params, write: params }
     } else {
-      throw new Error("LifeCycle::computed not given a valid type.")
+      throw new Error('LifeCycle::computed not given a valid type.')
     }
+
     params.disposeWhenNodeIsRemoved = this[ANCHOR_NODE]
     return this.addDisposable(computed(params))
   }
 
-  addEventListener(...args) {
+  addEventListener (...args) {
     const node = args[0].nodeType ? args.shift() : this[ANCHOR_NODE]
     const [type, act, options] = args
     const handler = typeof act === 'string' ? this[act].bind(this) : act
     this.__addEventListener(node, type, handler, options)
   }
 
-  __addEventListener(node, event_type, handler, options) {
-    node.addEventListener(event_type, handler, options)
-    function dispose() { node.removeEventListener(event_type, handler) }
+  __addEventListener (node, eventType, handler, options) {
+    node.addEventListener(eventType, handler, options)
+    function dispose () { node.removeEventListener(eventType, handler) }
     addDisposeCallback(node, dispose)
     this.addDisposable({ dispose })
   }
 
-  anchorTo(node) {
+  anchorTo (node) {
     addDisposeCallback(node, () => this.dispose())
     this[ANCHOR_NODE] = node
   }
 
-  dispose() {
+  dispose () {
     const subscriptions = this[SUBSCRIPTIONS] || []
     subscriptions.forEach(s => s.dispose())
     this[SUBSCRIPTIONS] = []
     this[ANCHOR_NODE] = null
-
   }
 
-  addDisposable(subscription) {
+  addDisposable (subscription) {
     const subscriptions = this[SUBSCRIPTIONS] || []
     if (!this[SUBSCRIPTIONS]) { this[SUBSCRIPTIONS] = subscriptions }
     if (typeof subscription.dispose !== 'function') {
-      throw new Error("Lifecycle::addDisposable argument missing `dispose`.")
+      throw new Error('Lifecycle::addDisposable argument missing `dispose`.')
     }
     subscriptions.push(subscription)
     return subscription
   }
-
 }
