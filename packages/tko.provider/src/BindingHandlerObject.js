168d82a74ee47dc8aef9c6e833d03b40815bcf67
Brian M Hunt <brianmhunt@gmail.com>
2017-05-11 17:45:45 -0400

    binding handlers) Respect `[handler].[attribute]`
    Dot-notation for e.g. `attr.title` or `event.click`

--- packages/tko.provider/src/BindingHandlerObject.js
+++ packages/tko.provider/src/BindingHandlerObject.js
@@ -25,7 +25,12 @@ export default class BindingHandlerObject {
     }
   }
 
-  get(name) {
+  /**
+   * The handler may have a `.` in it, e.g. `attr.title`, in which case the
+   * handler is `attr`.  Otherwise it's the name given
+   */
+  get(nameOrDotted) {
+    const [name] = nameOrDotted.split('.')
     return this[name]
   }
 }
