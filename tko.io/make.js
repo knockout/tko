ec6870a864ccea991d27d86dce06e1816a470c34
Brian M Hunt <bmh@tko.io>
2017-11-05 07:29:03 -0500

    Add making, watching, and fix the css grid
    Signed-off-by: Brian M Hunt <bmh@tko.io>

--- tko.io/make.js
+++ tko.io/make.js
@@ -3,8 +3,8 @@
 const fs = require('fs-extra')
 const yaml = require('js-yaml')
 const pug = require('pug')
+const debounce = require('lodash.debounce')
 var matter = require('gray-matter');
-
 
 const md = require('markdown-it')({
 	html: true,
@@ -23,28 +23,45 @@ function * genParts(sources) {
 	for (const sourceMd of config.sources) {
 		const source = fs.readFileSync(sourceMd, ENC)
 		const {content, data} = matter(source)
-		const asHtml = md.render(content)
-		yield asHtml
+		const html = md.render(content)
+		yield {html, data}
 	}
 }
 
-/**
- * Make build/
- */
-fs.mkdirs('build/')
+function make() {
 
-/**
- * Make build/index.html
- */
-console.log("Making build/index.html")
-const body = Array.from(genParts(config.sources))
-	.join("\n<!-- ---- ---- ---- -->")
+	/**
+	 * Make build/
+	 */
+	fs.mkdirs('build/')
 
-const html = pug.renderFile('src/index.pug', {body})
-const df = fs.writeFileSync(config.dest, html)
+	/**
+	 * Make build/index.html
+	 */
+	console.log("Making build/index.html")
+	const parts = Array.from(genParts(config.sources))
+	const html = pug.renderFile('src/index.pug', {parts})
+	const df = fs.writeFileSync(config.dest, html)
 
-/**
- * Make build/tko.css
- */
-console.log("Making build/tko.css")
-fs.copySync('static/tko.css', 'build/tko.css')
+	/**
+	 * Make build/tko.css
+	 */
+	console.log("Making build/tko.css")
+	fs.copySync('src/tko.css', 'build/tko.css')
+
+
+	/**
+	 * Make the Javascript
+	 */
+	console.log("Making build/tko-io.js")
+	fs.copySync('src/tko-io.js', 'build/tko-io.js')
+}
+
+if (process.argv.includes('-w')) {
+	const ignored = "" ///(^|[\/\\])(\..|build\/*)/
+	require('chokidar')
+		.watch('src', {ignored})
+		.on('all', debounce(make, 50))
+} else {
+	make()
+}
