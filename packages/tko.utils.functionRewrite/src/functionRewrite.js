e6f5cd68cfd996984fdfe79ae33349d7bee994c5
Brian M Hunt <bmh@tko.io>
2018-01-19 09:00:31 -0500

    packages) Fix configs for tko.functionRewrite and tko.builder

--- packages/tko.utils.functionRewrite/src/functionRewrite.js
+++ packages/tko.utils.functionRewrite/src/functionRewrite.js
@@ -2,24 +2,6 @@
  * This is a BindingString subclass that converts `function () {}` to lambas,
  * for backwards compatibility.
  *
- //  * Use with e.g.
- // class VirtualProviderWithFunctionRewrite extends VirtualProvider {
- //   preProcessBindings (bindingStringOrObjects) {
- //     if (typeof bindingStringOrObjects === 'string') {
- //       bindingStringOrObjects = functionRewrite(bindingStringOrObjects)
- //     }
- //     super.preProcessBindings(bindingStringOrObjects)
- //   }
- // }
- //
- // class DataBindProviderWithFunctionRewrite extends DataBindProvider {
- //   preProcessBindings (bindingStringOrObjects) {
- //     if (typeof bindingStringOrObjects === 'string') {
- //       bindingStringOrObjects = functionRewrite(bindingStringOrObjects)
- //     }
- //     super.preProcessBindings(bindingStringOrObjects)
- //   }
- // }
  */
 
 const FUNCTION_REX = /\bfunction\s*\(\s*\)\s*{\s*(?:return\s+([^}]+?)\s*)?}/g
