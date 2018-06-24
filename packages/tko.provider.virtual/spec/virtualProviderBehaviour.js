3421af15c9a6a2c2a1e10c76f1fb4d915cd99d64
Brian M Hunt <bmh@tko.io>
2018-06-24 11:51:17 -0400

    Add support for `<ko>` elements; closes #75

--- packages/tko.provider.virtual/spec/virtualProviderBehaviour.js
+++ packages/tko.provider.virtual/spec/virtualProviderBehaviour.js
@@ -17,4 +17,55 @@ describe('Virtual Provider', function () {
     const bindings = provider.getBindingAccessors(node.childNodes[0], ctx)
     assert.equal(bindings.text(), 'ax')
   })
+
+  it('binds multiple bindings as expected', function () {
+    const node = document.createElement('div')
+    node.innerHTML = '<!-- ko text: x, t2: "vx" --><!-- /ko -->'
+    const provider = new VirtualProvider()
+    const ctx = { lookup (v) { return 'ax' } }
+    const bindings = provider.getBindingAccessors(node.childNodes[0], ctx)
+    assert.equal(bindings.text(), 'ax')
+    assert.equal(bindings.t2(), 'vx')
+  })
+
+  describe('the <ko> element', function () {
+    function tryKoConvert (attributes, node = document.createElement('ko')) {
+      const provider = new VirtualProvider()
+      const parent = document.createElement('div')
+      for (const [name, value] of Object.entries(attributes)) {
+        node.setAttribute(name, value)
+      }
+      parent.appendChild(node)
+      return provider.preprocessNode(node)
+    }
+
+    it('converts <ko test=x>', function () {
+      const [open, close] = tryKoConvert({ test: 'x' })
+      assert.equal(open.nodeValue, 'ko test: x')
+      assert.equal(close.nodeValue, '/ko')
+    })
+
+    it('converts <ko t1=x, t2="y">', function () {
+      const [open, close] = tryKoConvert({ t1: 'x', 't2': '"Y"' })
+      assert.equal(open.nodeValue, 'ko t1: x,t2: "Y"')
+      assert.equal(close.nodeValue, '/ko')
+    })
+
+    it('replaces "ko-" prefix', function () {
+      const [open, close] = tryKoConvert({ 'ko-t1': 'x', 'ko-t2': '"Y"' })
+      assert.equal(open.nodeValue, 'ko t1: x,t2: "Y"')
+      assert.equal(close.nodeValue, '/ko')
+    })
+
+    it('includes child nodes in the new virtual element', function () {
+      const koNode = document.createElement('ko')
+      koNode.appendChild(document.createElement('div'))
+      koNode.appendChild(document.createComment('abcc'))
+      const [open, d, c, close] = tryKoConvert({ t1: 'x', 't2': '"Y"' }, koNode)
+      assert.equal(open.nodeValue, 'ko t1: x,t2: "Y"')
+      assert.equal(close.nodeValue, '/ko')
+      assert.equal(d.outerHTML, '<div></div>')
+      assert.equal(c.nodeValue, 'abcc')
+    })
+  })
 })
--- packages/tko.provider.virtual/src/VirtualProvider.js
+++ packages/tko.provider.virtual/src/VirtualProvider.js
@@ -7,7 +7,34 @@ import {
 } from 'tko.provider.bindingstring'
 
 export default class VirtualProvider extends BindingStringProvider {
-  get FOR_NODE_TYPES () { return [ 8 ] } // document.COMMENT_NODE
+  get FOR_NODE_TYPES () { return [ 1, 8 ] }
+
+  /**
+   * Convert <ko binding='...'> into <!-- ko binding: ... -->
+   * @param {HTMLElement} node
+   */
+  preprocessNode (node) {
+    if (node.tagName === 'KO') {
+      const parent = node.parentNode
+      const childNodes = [...node.childNodes]
+      const virtualBindingString = [...this.genElementBindingStrings(node)].join(',')
+      const openNode = document.createComment('ko ' + virtualBindingString)
+      const closeNode = document.createComment('/ko')
+      parent.insertBefore(openNode, node)
+      for (const child of childNodes) {
+        parent.insertBefore(child, node)
+      }
+      parent.insertBefore(closeNode, node)
+      node.remove()
+      return [openNode, ...childNodes, closeNode]
+    }
+  }
+
+  * genElementBindingStrings (node) {
+    for (const {name, value} of node.attributes) {
+      yield `${name.replace(/^ko-/, '')}: ${value}`
+    }
+  }
 
   getBindingString (node) {
     if (node.nodeType === document.COMMENT_NODE) {
