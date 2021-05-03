9a5f68abb47a3f49d0f461263beba6e1076766ab
Brian M Hunt <brian@minutebox.com>
2021-05-02 22:01:01 -0400

    Fix tests hanging b/c packages/builder had none

---
+++ packages/builder/spec/builderBehaviors.ts
@@ -0,0 +1,18 @@
+import { VirtualProvider } from '@tko/provider.virtual'
+import { bindings as ifBindings } from '@tko/binding.if'
+
+import { Builder } from '../dist'
+
+describe('Builder', () => {
+  it('creates a ko instance', () => {
+    // We're just testing that the builder constructs, here.
+    const builder = new Builder({
+      filters: {},
+      provider: new VirtualProvider(),
+      bindings: [
+        ifBindings,
+      ],
+      options: {},
+    })
+  })
+})
