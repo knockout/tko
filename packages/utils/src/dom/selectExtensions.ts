844649c7e4697bc9af3be8a969b2b9ba4ef1b71f
Brian M Hunt <brian@minutebox.com>
2022-01-09 12:08:45 -0500

    utils/dom) remove ie6 workaround to selectExtension

--- packages/utils/src/dom/selectExtensions.ts
+++ packages/utils/src/dom/selectExtensions.ts
@@ -64,12 +64,6 @@ export var selectExtensions = {
         }
         if (allowUnset || selection >= 0 || (value === undefined && element.size > 1)) {
           element.selectedIndex = selection
-          if (ieVersion === 6) {
-            // Workaround for IE6 bug: It won't reliably apply values to SELECT nodes during the same execution thread
-            // right after you've changed the set of OPTION nodes on it. So for that node type, we'll schedule a second thread
-            // to apply the value as well.
-            safeSetTimeout(() => { element.selectedIndex = selection }, 0)
-          }
         }
         break
       default:
