b1f3b5a1367af4ef498057cc8969e1863838d077
Auge <steffenaugustin88@gmail.com>
2025-01-14 08:45:11 +0100

    ConditionalBindingHandler

--- packages/binding.if/src/ConditionalBindingHandler.ts
+++ packages/binding.if/src/ConditionalBindingHandler.ts
@@ -9,6 +9,7 @@ import {
 import {
     applyBindingsToDescendants, AsyncBindingHandler
 } from '@tko/bind'
+import { Observable } from 'node_modules/@tko/observable/types/Observable';
 
 /**
  * Create a DOMbinding that controls DOM nodes presence
