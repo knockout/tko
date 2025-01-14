b07e22a37504d032bc88efff5c6d2cf425a2720a
Auge <steffenaugustin88@gmail.com>
2025-01-14 08:52:05 +0100

    optionsBehaviors fixed tsc

--- packages/binding.component/spec/componentBindingBehaviors.ts
+++ packages/binding.component/spec/componentBindingBehaviors.ts
@@ -42,6 +42,7 @@ import components from '@tko/utils.component'
 import {
     useMockForTasks
 } from '@tko/utils/helpers/jasmine-13-helper'
+import { ObservableArray } from 'packages/observable/types/Observable'
 
 
 
@@ -1396,7 +1397,7 @@ describe('Components: Component binding', function () {
 
     it('respects observable array changes with JSX', function () {
       testNode.innerHTML = ''
-      const arr = observableArray([])
+      const arr: ObservableArray<any> = observableArray([])
 
       // <test-component>
       //   <template slot='X'>{arr}</template>
--- packages/binding.core/spec/optionsBehaviors.ts
+++ packages/binding.core/spec/optionsBehaviors.ts
@@ -25,6 +25,7 @@ import '@tko/utils/helpers/jasmine-13-helper'
 import {
     matchers
 } from '../dist/test-helper'
+import { Observable, ObservableArray } from 'packages/observable/types/Observable'
 
 declare var testNode : HTMLElement
 
@@ -183,7 +184,7 @@ describe('Binding: Options', function () {
   })
 
   it('Should trigger a change event when the options selection is populated or changed by modifying the options data (single select)', function () {
-    var myObservable = observableArray(['A', 'B', 'C']), changeHandlerFireCount = 0
+    var myObservable: ObservableArray<string | number> = observableArray(['A', 'B', 'C']), changeHandlerFireCount = 0
     testNode.innerHTML = "<select data-bind='options:myValues'></select>"
     registerEventHandler(testNode.childNodes[0], 'change', function () {
       changeHandlerFireCount++
@@ -214,7 +215,7 @@ describe('Binding: Options', function () {
   })
 
   it('Should trigger a change event when the options selection is changed by modifying the options data (multiple select)', function () {
-    var myObservable = observableArray(['A', 'B', 'C']), changeHandlerFireCount = 0
+    var myObservable: ObservableArray<any> = observableArray(['A', 'B', 'C']), changeHandlerFireCount = 0
     testNode.innerHTML = "<select data-bind='options:myValues' multiple='multiple'></select>"
     registerEventHandler(testNode.childNodes[0], 'change', function () {
       changeHandlerFireCount++
