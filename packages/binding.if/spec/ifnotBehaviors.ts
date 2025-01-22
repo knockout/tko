15b27e7381c08f1a70429dafeb588cdbe617feed
mcselle <sellemarcel@gmail.com>
2025-01-22 11:58:47 +0100

    comment unused tests

--- packages/binding.if/spec/ifnotBehaviors.ts
+++ packages/binding.if/spec/ifnotBehaviors.ts
@@ -44,14 +44,14 @@ describe('Binding: Ifnot', function () {
   })
 
   xit('Should leave descendant nodes in the document (and bind them) if the value is falsy, independently of the active template engine', function () {
-    this.after(function () { setTemplateEngine(new nativeTemplateEngine()) })
+    // this.after(function () { setTemplateEngine(new nativeTemplateEngine()) })
 
-    setTemplateEngine(new nativeTemplateEngine()) // This template engine will just throw errors if you try to use it
-    testNode.innerHTML = "<div data-bind='ifnot: condition'><span data-bind='text: someItem.existentChildProp'></span></div>"
-    expect(testNode.childNodes.length).toEqual(1)
-    applyBindings({ someItem: { existentChildProp: 'Child prop value' }, condition: false }, testNode)
-    expect(testNode.childNodes[0].childNodes.length).toEqual(1)
-    expect(testNode.childNodes[0].childNodes[0]).toContainText('Child prop value')
+    // setTemplateEngine(new nativeTemplateEngine()) // This template engine will just throw errors if you try to use it
+    // testNode.innerHTML = "<div data-bind='ifnot: condition'><span data-bind='text: someItem.existentChildProp'></span></div>"
+    // expect(testNode.childNodes.length).toEqual(1)
+    // applyBindings({ someItem: { existentChildProp: 'Child prop value' }, condition: false }, testNode)
+    // expect(testNode.childNodes[0].childNodes.length).toEqual(1)
+    // expect(testNode.childNodes[0].childNodes[0]).toContainText('Child prop value')
   })
 
   it('Should leave descendant nodes unchanged if the value is falsy and remains falsy when changed', function () {
