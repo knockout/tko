d3f04d72e6f9975a97620fdbbacc4f7fa313c46d
phillipc <phillipc@gmx.de>
2025-02-05 09:41:50 +0100

    test) fix test-case for electron

--- packages/binding.core/spec/hasfocusBehaviors.ts
+++ packages/binding.core/spec/hasfocusBehaviors.ts
@@ -7,7 +7,8 @@ import {
 } from '@tko/bind'
 
 import {
-    observable
+  observable, unwrap, dependencyDetection,
+  isWriteableObservable
 } from '@tko/observable'
 
 import {
@@ -18,22 +19,94 @@ import {
     options, arrayForEach
 } from '@tko/utils'
 
+
 import * as coreBindings from '../dist'
 
 import '@tko/utils/helpers/jasmine-13-helper'
+import { VirtualProvider } from '@tko/provider.virtual'
+import { MultiProvider } from '@tko/provider.multi'
 
 
-arrayForEach(['hasfocus', 'hasFocus'], binding => {
+var hasfocusUpdatingProperty = '__ko_hasfocusKnockoutUpdating';
+var hasfocusLastValue = '__ko_hasfocusKnockoutLastValue';
+
+function writeValueToProperty (property: any, value: any, checkIfDifferent: boolean) {
+  if (isWriteableObservable(property) && (!checkIfDifferent || property.peek() !== value)) {
+      property.valueHasMutated()
+  }
+}
+
+arrayForEach(['hasfocus', 'hasFocus', 'focusKnockout351'], binding => {
   describe(`Binding: ${binding}`, function () {
     var bindingHandlers
 
     beforeEach(jasmine.prepareTestNode)
 
     beforeEach(function () {
-      var provider = new DataBindProvider()
+
+      var provider = new MultiProvider({
+        providers: [new DataBindProvider(), new VirtualProvider()]
+      })
       options.bindingProviderInstance = provider
       bindingHandlers = provider.bindingHandlers
-      bindingHandlers.set(coreBindings.bindings)
+      bindingHandlers.set(coreBindings.bindings)      
+      
+
+      bindingHandlers.focusKnockout351 = {
+        'init': function(element, valueAccessor, allBindings) {
+            var handleElementFocusChange = function(isFocused) {
+    
+                element[hasfocusUpdatingProperty] = true;
+                var ownerDoc = element.ownerDocument;
+                if ("activeElement" in ownerDoc) {
+                    var active;
+                    try {
+                        active = ownerDoc.activeElement;
+                    } catch(e) {
+                        // IE9 throws if you access activeElement during page load (see issue #703)
+                        active = ownerDoc.body;
+                    }
+                    isFocused = (active === element);
+                }
+                var modelValue = valueAccessor(isFocused, {onlyIfChanged: true});
+                if (isWriteableObservable(modelValue) && (modelValue.peek() !== isFocused)) {
+                   modelValue(isFocused);
+                }
+    
+                //cache the latest value, so we can avoid unnecessarily calling focus/blur in the update function
+                element[hasfocusLastValue] = isFocused;
+                element[hasfocusUpdatingProperty] = false;
+            };
+            var handleElementFocusIn = handleElementFocusChange.bind(null, true);
+            var handleElementFocusOut = handleElementFocusChange.bind(null, false);
+    
+            registerEventHandler(element, "focus", handleElementFocusIn);
+            registerEventHandler(element, "focusin", handleElementFocusIn); // For IE
+            registerEventHandler(element, "blur",  handleElementFocusOut);
+            registerEventHandler(element, "focusout",  handleElementFocusOut); // For IE
+    
+            // Assume element is not focused (prevents "blur" being called initially)
+            element[hasfocusLastValue] = false;
+        },
+        'update': function(element, valueAccessor) {
+            var value = !!unwrap(valueAccessor());
+    
+            if (!element[hasfocusUpdatingProperty] && element[hasfocusLastValue] !== value) {
+                value ? element.focus() : element.blur();
+    
+                // In IE, the blur method doesn't always cause the element to lose focus (for example, if the window is not in focus).
+                // Setting focus to the body element does seem to be reliable in IE, but should only be used if we know that the current
+                // element was focused already.
+                if (!value && element[hasfocusLastValue]) {
+                    element.ownerDocument.body.focus();
+                }
+    
+                // For IE, which doesn't reliably fire "focus" or "blur" events synchronously
+                dependencyDetection.ignore(triggerEvent, null, [element, value ? "focusin" : "focusout"]);
+            }
+        }
+      };
+
     })
 
     if (ieVersion) {
@@ -122,7 +195,8 @@ arrayForEach(['hasfocus', 'hasFocus'], binding => {
   arrayForEach(['beforeChange', 'change', 'spectate', 'asleep', 'awake'], event => {
     it('Modern browser: non-observable and Observerble value to be true on focus and false on blur', function () {
       var model = { myVal: observable(false),  myVal2: observable(false)  }
-      var displayVal = observable(undefined)
+      const obj : any = undefined
+      var displayVal = observable(obj)
       testNode.innerHTML = `<input class='myVal' data-bind='${binding}: myVal' /><input class='myVal2' data-bind='${binding}: myVal2' />`
       applyBindings(model, testNode);
 
@@ -132,21 +206,21 @@ arrayForEach(['hasfocus', 'hasFocus'], binding => {
       let changeCounter = 0;
       
       defineSubscription(model.myVal, (newValue) => {
-        //console.log('fire -' + event + ' myVal:' + newValue + ' / ' + model.myVal() + ' / ' +  doc.activeElement?.tagName + ' ' + doc.activeElement?.className)  
+        console.log('fire -' + event + ' myVal:' + newValue + ' / ' + model.myVal() + ' / ' +  doc.activeElement?.tagName + ' ' + doc.activeElement?.className)  
         if (newValue !== displayVal()) {
             displayVal(newValue);
           }
       }, false, event);
 
       defineSubscription(model.myVal2, (newValue) => {
-        //console.log('fire -' + event + ' myVal2:' + newValue + ' / ' + model.myVal2() + ' / ' +  doc.activeElement?.tagName + ' ' + doc.activeElement?.className)  
+        console.log('fire -' + event + ' myVal2:' + newValue + ' / ' + model.myVal2() + ' / ' +  doc.activeElement?.tagName + ' ' + doc.activeElement?.className)  
         if (newValue !== displayVal()) {
             displayVal(newValue);
           }
       }, false, event);
 
       defineSubscription(displayVal, (newValue) => {
-        //console.log('fire -' + event + ' displayVal:' + newValue)          
+        console.log('fire -' + event + ' displayVal:' + newValue)          
         changeCounter++
       }, false, 'change');
 
@@ -154,26 +228,28 @@ arrayForEach(['hasfocus', 'hasFocus'], binding => {
       expect(model.myVal()).toEqual(false);
       expect(model.myVal2()).toEqual(false);
 
-      //console.log('focus input 0-myVal')
+      console.log(binding + ': focus input 0-myVal ' + event)
       input0.focus();
-      //triggerEvent(testNode.children[0], 'focusin')
+      triggerEvent(input0, 'focusin')
       expect(model.myVal()).toEqual(true);
       expect(model.myVal2()).toEqual(false);
 
-      //console.log('focusout input 0-myVal')
+      console.log('focusout input 0-myVal')
       input0.blur();
+      triggerEvent(input0, 'focusout')
       expect(model.myVal()).toEqual(false);
       expect(model.myVal2()).toEqual(false);
 
       // Move the focus elsewhere
-      //console.log('focus input 1-myVal2')
+      console.log('focus input 1-myVal2')
       input1.focus();
-      //triggerEvent(testNode.children[0], 'focusout')
+      triggerEvent(input1, 'focusout')
       expect(model.myVal()).toEqual(false)
       expect(model.myVal2()).toEqual(true);
 
-      //console.log('focusout input 1-myVal2')
+      console.log('focusout input 1-myVal2')
       input1.blur();
+      triggerEvent(input1, 'focusout')
       expect(model.myVal()).toEqual(false);
       expect(model.myVal2()).toEqual(false);
 
@@ -185,7 +261,8 @@ arrayForEach(['hasfocus', 'hasFocus'], binding => {
 
     it('Should not unnecessarily focus or blur an element that is already focused/blurred', function () {
           // This is the closest we can get to representing issue #698 as a spec
-      var model = { isFocused: observable({}) }
+      const obj : any = {}
+      var model = { isFocused: observable(obj) }
       testNode.innerHTML = `<input data-bind='${binding}: isFocused' />`
       applyBindings(model, testNode)
 
