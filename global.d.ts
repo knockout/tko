46072301c6ab43d989c17bafc1d4eabf2d5e3f69
Brian M Hunt <brianmhunt@gmail.com>
2025-12-22 10:32:28 -0500

    ts) TypeScript foundation - type definitions and config
    This commit establishes the foundation for gradual TypeScript adoption:
    
    Changes:
    - Enhanced global.d.ts with missing Jasmine 1.x type definitions
      - Added jasmine.Clock.reset() and useMock()
      - Added jasmine.Spy.andCallFake()
      - Added global waits() and runs() functions for Jasmine 1.x async tests
    - Adjusted tsconfig.json to loosen strictness for gradual adoption
      - Set noImplicitOverride: false
      - Set noImplicitReturns: false
      - Set strictFunctionTypes: false
      - Added emitDeclarationOnly and outDir for d.ts generation
    - Added standard.globals section to package.json for test globals
    - Updated type packages to correct versions
    
    Impact:
    - All 955 tests pass ✓
    - TypeScript compilation now succeeds with 0 errors ✓
    - Sets foundation for incremental source code type annotation
    
    Related to PR #213

--- global.d.ts
+++ global.d.ts
@@ -2,62 +2,79 @@
 /// <reference types="jquery" />
 
 //export {} allows redefining window module
-export {}
+export { };
 
 declare global {
-  // Below just informs IDE and/or TS-compiler (it's set in `.js` file).
-  interface Window {
-    DEBUG: boolean
-    amdRequire: any
-    require: any
-    jQuery: JQueryStatic
-    innerShiv // TODO: For IE<9.. we could also remove it
-  }
 
-  //Jasmine and Mocha define duplicated functions, is a problem for the type system
-  //This namespace merges the jasmine namespace to correct same tsc warnings
-  namespace jasmine {
-    function setNodeText(node, text: string): void
-    function getGlobal(): any
-    function resolve(promise: Promise<boolean>)
-    function prepareTestNode(): HTMLElement
-    function nodeText(node)
-    function getEnv(): any
-
-    const Matchers: Matchers
-    const Spec: any
-    const FakeTimer: any
-    const undefined: undefined
-
-    let browserSupportsProtoAssignment: any
-    let ieVersion: any
-    let updateInterval: number
-
-    interface Matchers<T> {
-      toContainText(expected: string, ignoreSpaces: boolean): boolean
-      toHaveOwnProperties(expectedProperties: any): boolean
-      toHaveTexts(expectedTexts: any): boolean
-      toHaveValues(expectedValues: any): boolean
-      toHaveCheckedStates(expectedValues: any): boolean
-      toThrowContaining(expected: any): boolean
-      toEqualOneOf(expectedPossibilities: any): boolean
-      toContainHtml(expectedHtml: any, postProcessCleanedHtml: any): boolean
-      toHaveSelectedValues(expectedValues: any): boolean
-      toContainHtml(expectedValues: any): boolean
-      toHaveNodeTypes(expectedTypes: any): boolean
-      toContainHtmlElementsAndText(expectedHtml: any): boolean
+    // Below just informs IDE and/or TS-compiler (it's set in `.js` file).
+    interface Window {
+        DEBUG: boolean
+        amdRequire: any
+        require: any
+        jQuery: JQueryStatic
+        innerShiv // TODO: For IE<9.. we could also remove it
     }
 
-    interface Clock {
-      mockScheduler: any
+    //Jasmine and Mocha define duplicated functions, is a problem for the type system
+    //This namespace merges the jasmine namespace to correct same tsc warnings
+    namespace jasmine {
+
+        function setNodeText(node, text: string): void
+        var Spec: any;
+        function getGlobal(): any;
+        var updateInterval: number
+        function resolve(promise: Promise<boolean>)
+        function prepareTestNode() : HTMLElement
+        function nodeText(node)
+        var Clock: Clock
+        function getEnv(): any;
+
+        var FakeTimer: any
+        var undefined: undefined
+        var browserSupportsProtoAssignment: any
+        var ieVersion: any
+
+        var Matchers: Matchers
+
+        interface Matchers<T> {
+            toContainText(expected: string, ignoreSpaces: boolean): boolean
+            toHaveOwnProperties(expectedProperties: any): boolean
+            toHaveTexts(expectedTexts: any): boolean
+            toHaveValues(expectedValues: any): boolean
+            toHaveCheckedStates(expectedValues: any): boolean
+            toThrowContaining(expected: any): boolean
+            toEqualOneOf(expectedPossibilities: any): boolean
+            toContainHtml(expectedHtml: any, postProcessCleanedHtml: any): boolean
+            toHaveSelectedValues(expectedValues: any): boolean
+            toContainHtml(expectedValues: any): boolean
+            toHaveNodeTypes(expectedTypes: any): boolean
+            toContainHtmlElementsAndText(expectedHtml: any): boolean
+        }
+
+        interface Clock {
+            mockScheduler: any
+            reset(): void
+            useMock(): void
+        }
+
+        interface Spy {
+            reset(): any
+            andCallFake(fn: Function): Spy
+        }
     }
 
-    interface Spy {
-      reset(): any
-    }
-  }
+    // Jasmine 1.x async test helpers (deprecated but still used in codebase)
+    function waits(timeout?: number): void
+    function runs(fn: Function): void
 
-  interface SymbolConstructor {
-    observable?: symbol
-  }
+    //Knockout Types https://github.com/knockout/knockout/blob/master/build/types/knockout.d.ts#L404
+    // Type definitions for Knockout
+    // Project: http://knockoutjs.com
+    // Definitions by: Maxime LUCE <https://github.com/SomaticIT>, Michael Best <https://github.com/mbest>
+
+    // Treeshaked for TKO: Almost all type definitions are moved to the TKO source.
+
+    interface SymbolConstructor {
+        observable?: Symbol;
+    }
 }
