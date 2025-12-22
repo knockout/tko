9dca7c1001994c12229a7f579bfa4c1b01e3e5f6
P.Conrad <phillipc@gmx.de>
2025-12-22 09:32:08 +0000

    ts) Node constants instead of numeric literals

--- packages/utils/src/dom/html.ts
+++ packages/utils/src/dom/html.ts
@@ -217,7 +217,7 @@ export function setTextContent(element: Node, textContent?: TextContent): void {
   // If there are no children, more than one, or if it's not a text node,
   // we'll clear everything and create a single text node.
   let innerTextNode = virtualElements.firstChild(element)
-  if (!innerTextNode || innerTextNode.nodeType != 3 || virtualElements.nextSibling(innerTextNode)) {
+  if (!innerTextNode || innerTextNode.nodeType !== Node.TEXT_NODE || virtualElements.nextSibling(innerTextNode)) {
     virtualElements.setDomNodeChildren(element, [element.ownerDocument!.createTextNode(value)])
   } else {
     ;(innerTextNode as Text).data = value
