75ffdf6b78dbdc17bc5d558865cdcdf0b2539a7e
P.Conrad <phillipc@gmx.de>
2026-01-13 22:50:10 +0000

    format) execute prettier

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -29,10 +29,5 @@ import * as tasks from './tasks'
 import * as virtualElements from './dom/virtualElements'
 import * as domData from './dom/data'
 
-export type {  
-  IProvider,
-  BindingAccessors,
-  KnockoutUtils,
-  ArrayAndObjectUtils
-} from './interfaces'
+export type { IProvider, BindingAccessors, KnockoutUtils, ArrayAndObjectUtils } from './interfaces'
 export { tasks, virtualElements, domData, memoization }
