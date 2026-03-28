35b990ff0bcdc7f8910d180c8e0df2399a681274
Brian M Hunt <brianmhunt@gmail.com>
2026-03-28 10:36:35 -0400

    Convert binding.if tests to Mocha

--- packages/binding.if/spec/ifBehaviors.ts
+++ packages/binding.if/spec/ifBehaviors.ts
@@ -1,5 +1,6 @@
 /* globals testNode */
 import { applyBindings, contextFor } from '@tko/bind'
+import { expect } from 'chai'
 
 import { observable } from '@tko/observable'
 
@@ -18,12 +19,13 @@ import {
 
 import { bindings as coreBindings } from '@tko/binding.core'
 
-import '@tko/utils/helpers/jasmine-13-helper'
+import { expectContainHtml, expectContainText, prepareTestNode } from '../../utils/helpers/mocha-test-helpers'
 
 describe('Binding: If', function () {
   let testNode: HTMLElement
+
   beforeEach(function () {
-    testNode = jasmine.prepareTestNode()
+    testNode = prepareTestNode()
   })
 
   beforeEach(function () {
@@ -36,9 +38,9 @@ describe('Binding: If', function () {
   it('Should remove descendant nodes from the document (and not bind them) if the value is falsy', function () {
     testNode.innerHTML =
       "<div data-bind='if: someItem'><span data-bind='text: someItem.nonExistentChildProp'></span></div>"
-    expect(testNode.childNodes[0].childNodes.length).toEqual(1)
+    expect(testNode.childNodes[0].childNodes.length).to.equal(1)
     applyBindings({ someItem: null }, testNode)
-    expect(testNode.childNodes[0].childNodes.length).toEqual(0)
+    expect(testNode.childNodes[0].childNodes.length).to.equal(0)
   })
 
   xit('Should leave descendant nodes in the document (and bind them) if the value is truthy, independently of the active template engine', function () {
@@ -56,17 +58,15 @@ describe('Binding: If', function () {
     testNode.innerHTML = "<div data-bind='if: someItem'><span data-bind='text: ++counter'></span></div>"
     const originalNode = testNode.childNodes[0].childNodes[0]
 
-    // Value is initially true, so nodes are retained
     applyBindings({ someItem, counter: 0 }, testNode)
-    expect((testNode.childNodes[0].childNodes[0] as HTMLElement).tagName.toLowerCase()).toEqual('span')
-    expect(testNode.childNodes[0].childNodes[0]).toEqual(originalNode)
-    expect(testNode).toContainText('1')
+    expect((testNode.childNodes[0].childNodes[0] as HTMLElement).tagName.toLowerCase()).to.equal('span')
+    expect(testNode.childNodes[0].childNodes[0]).to.equal(originalNode)
+    expectContainText(testNode, '1')
 
-    // Change the value to a different truthy value; see the previous SPAN remains
     someItem('different truthy value')
-    expect((testNode.childNodes[0].childNodes[0] as HTMLElement).tagName.toLowerCase()).toEqual('span')
-    expect(testNode.childNodes[0].childNodes[0]).toEqual(originalNode)
-    expect(testNode).toContainText('1')
+    expect((testNode.childNodes[0].childNodes[0] as HTMLElement).tagName.toLowerCase()).to.equal('span')
+    expect(testNode.childNodes[0].childNodes[0]).to.equal(originalNode)
+    expectContainText(testNode, '1')
   })
 
   it('Should toggle the presence and bindedness of descendant nodes according to the truthiness of the value', function () {
@@ -75,65 +75,59 @@ describe('Binding: If', function () {
       "<div data-bind='if: someItem'><span data-bind='text: someItem().occasionallyExistentChildProp'></span></div>"
     applyBindings({ someItem }, testNode)
 
-    // First it's not there
-    expect(testNode.childNodes[0].childNodes.length).toEqual(0)
+    expect(testNode.childNodes[0].childNodes.length).to.equal(0)
 
-    // Then it's there
     someItem({ occasionallyExistentChildProp: 'Child prop value' })
-    expect(testNode.childNodes[0].childNodes.length).toEqual(1)
-    expect(testNode.childNodes[0].childNodes[0]).toContainText('Child prop value')
+    expect(testNode.childNodes[0].childNodes.length).to.equal(1)
+    expectContainText(testNode.childNodes[0].childNodes[0], 'Child prop value')
 
-    // Then it's gone again
     someItem(null)
-    expect(testNode.childNodes[0].childNodes.length).toEqual(0)
+    expect(testNode.childNodes[0].childNodes.length).to.equal(0)
   })
 
   it('Should not interfere with binding context', function () {
     testNode.innerHTML = "<div data-bind='if: true'>Parents: <span data-bind='text: $parents.length'></span></div>"
     applyBindings({}, testNode)
-    expect(testNode.childNodes[0]).toContainText('Parents: 0')
-    expect(contextFor(testNode.childNodes[0].childNodes[1]).$parents.length).toEqual(0)
+    expectContainText(testNode.childNodes[0], 'Parents: 0')
+    expect(contextFor(testNode.childNodes[0].childNodes[1]).$parents.length).to.equal(0)
   })
 
-  it('Should be able to define an \"if\" region using a containerless template', function () {
+  it('Should be able to define an "if" region using a containerless template', function () {
     const someitem = observable(undefined)
     testNode.innerHTML =
       'hello <!-- ko if: someitem --><span data-bind="text: someitem().occasionallyexistentchildprop"></span><!-- /ko --> goodbye'
     applyBindings({ someitem }, testNode)
 
-    // First it's not there
-    expect(testNode).toContainHtml('hello <!-- ko if: someitem --><!-- /ko --> goodbye')
+    expectContainHtml(testNode, 'hello <!-- ko if: someitem --><!-- /ko --> goodbye')
 
-    // Then it's there
     someitem({ occasionallyexistentchildprop: 'child prop value' })
-    expect(testNode).toContainHtml(
+    expectContainHtml(
+      testNode,
       'hello <!-- ko if: someitem --><span data-bind="text: someitem().occasionallyexistentchildprop">child prop value</span><!-- /ko --> goodbye'
     )
 
-    // Then it's gone again
     someitem(null)
-    expect(testNode).toContainHtml('hello <!-- ko if: someitem --><!-- /ko --> goodbye')
+    expectContainHtml(testNode, 'hello <!-- ko if: someitem --><!-- /ko --> goodbye')
   })
 
-  it('Should be able to nest \"if\" regions defined by containerless templates', function () {
+  it('Should be able to nest "if" regions defined by containerless templates', function () {
     const condition1 = observable(false)
     const condition2 = observable(false)
     testNode.innerHTML =
       'hello <!-- ko if: condition1 -->First is true<!-- ko if: condition2 -->Both are true<!-- /ko --><!-- /ko -->'
     applyBindings({ condition1: condition1, condition2: condition2 }, testNode)
 
-    // First neither are there
-    expect(testNode).toContainHtml('hello <!-- ko if: condition1 --><!-- /ko -->')
+    expectContainHtml(testNode, 'hello <!-- ko if: condition1 --><!-- /ko -->')
 
-    // Make outer appear
     condition1(true)
-    expect(testNode).toContainHtml(
+    expectContainHtml(
+      testNode,
       'hello <!-- ko if: condition1 -->first is true<!-- ko if: condition2 --><!-- /ko --><!-- /ko -->'
     )
 
-    // Make inner appear
     condition2(true)
-    expect(testNode).toContainHtml(
+    expectContainHtml(
+      testNode,
       'hello <!-- ko if: condition1 -->first is true<!-- ko if: condition2 -->both are true<!-- /ko --><!-- /ko -->'
     )
   })
@@ -141,7 +135,6 @@ describe('Binding: If', function () {
   it('Should call a childrenComplete callback function', function () {
     testNode.innerHTML =
       "<div data-bind='if: condition, childrenComplete: callback'><span data-bind='text: someText'></span></div>"
-    //const someItem = observable({ childprop: 'child' })
     let callbacks = 0
     const viewModel = {
       condition: observable(true),
@@ -151,15 +144,15 @@ describe('Binding: If', function () {
       }
     }
     applyBindings(viewModel, testNode)
-    expect(callbacks).toEqual(1)
-    expect(testNode.childNodes[0]).toContainText('hello')
+    expect(callbacks).to.equal(1)
+    expectContainText(testNode.childNodes[0], 'hello')
 
     viewModel.condition(false)
-    expect(callbacks).toEqual(1)
-    expect(testNode.childNodes[0].childNodes.length).toEqual(0)
+    expect(callbacks).to.equal(1)
+    expect(testNode.childNodes[0].childNodes.length).to.equal(0)
 
     viewModel.condition(true)
-    expect(callbacks).toEqual(2)
-    expect(testNode.childNodes[0]).toContainText('hello')
+    expect(callbacks).to.equal(2)
+    expectContainText(testNode.childNodes[0], 'hello')
   })
 })
